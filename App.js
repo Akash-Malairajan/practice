7bbe7adeac45f2b46cc3c6670f12eec610f7758e
Akash1526 <akash.malairajan@gmail.com>
2024-09-12 10:35:14 +0530

    changes updated

--- App.js
+++ App.js
@@ -1,120 +1,12 @@
 
-// import React, { useState } from 'react';
-// import { View, Text, StyleSheet, Image, TouchableOpacity, Platform, Dimensions } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { Ionicons } from '@expo/vector-icons';
-// import FirstScreen from './firstScreen'; 
-
-// const { width, height } = Dimensions.get('window');
-// const Stack = createStackNavigator(); 
-
-// function ProgramDetailsScreen({ navigation }) {
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.programContainer}>
-//         <Image 
-//           source={require('./assets/img1.png')}
-//           style={styles.programImage} 
-//         />
-//         <Text style={styles.programTitle}>Time management</Text>
-//       </View>
-    
-//       <View style={styles.detailsContainer}>
-//         <Text style={styles.unlockedText}>Program unlocked!</Text>
-//         <Text style={styles.descriptionText}>
-//           Your Be future ready program has been successfully purchased
-//         </Text>
-
-//         <View style={styles.featureContainer}>
-//           <Text style={styles.featureText}>This program has 6 activities</Text>
-//         </View>
-//         <View style={styles.featureContainer}>
-//           <Text style={styles.featureText}>Full-time access to this program</Text>
-//         </View>
-//       </View>
-
-//       <View style={styles.continueContainer}>
-        
-//         <TouchableOpacity 
-//           style={styles.continueButton} 
-//           onPress={() => navigation.navigate('BeFutureReady')}
-//         >
-//           <Text style={styles.continueButtonText}>Continue</Text>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   );
-// }
-
-// export default function App() {
-//   const [isPlaying, setIsPlaying] = useState(false);
-
-//   const togglePlayPause = () => {
-//     setIsPlaying(!isPlaying);
-//   };
-
-//   const PlayPauseButton = () => (
-//     <TouchableOpacity onPress={togglePlayPause} style={styles.playPauseButton}>
-//       <Ionicons name={isPlaying ? "pause" : "play"} size={15} color="white" />
-//     </TouchableOpacity>
-//   );
-
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-      
-//         <Stack.Screen name="ProgramDetails" component={ProgramDetailsScreen} options={{ title: 'Program details' }} />
-//         <Stack.Screen 
-//           name="BeFutureReady" 
-//           component={FirstScreen} 
-//           options={{ 
-//             title: 'Be future ready', 
-//             headerRight: () => <PlayPauseButton /> 
-//           }} 
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
-// const styles = StyleSheet.create({
-//   container: { flex: 1, backgroundColor: '#f5f5f5', paddingTop: Platform.OS === 'ios' ? 15 : 0 },
-//   programContainer: { alignItems: 'center', marginBottom: 30, height: height * 0.4, width: '100%', backgroundColor: '#f5f5dc', borderBottomRightRadius: 150, borderBottomLeftRadius: 150 },
-//   programImage: { width: 250, height: 250, borderRadius: 20 },
-//   programTitle: { fontSize: 18, color: 'black', marginTop: 10, fontWeight: 'bold' },
-//   detailsContainer: { marginTop: 20, marginBottom: 20, backgroundColor: 'transparent', borderRadius: 10, paddingHorizontal: 20 },
-//   unlockedText: { fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginBottom: 15 },
-//   descriptionText: { textAlign: 'center', color: '#6c757d', marginBottom: 30, fontSize: 16 },
-//   featureContainer: { flexDirection: 'row', alignItems: 'center', paddingLeft: 30, marginBottom: 10 },
-//   featureText: { marginLeft: 10, color: '#6c757d', fontSize: 16 },
-//   continueContainer: { width: '100%', justifyContent: 'center', alignItems: 'center' },
-//   continueButton: { backgroundColor: '#FFA500', paddingVertical: 15, borderRadius: 25, alignItems: 'center', marginTop: 20, width: '85%' },
-//   continueButtonText: { color: 'black', fontWeight: 'bold', fontSize: 18 },
-//   playPauseButton: {
-//     backgroundColor: 'orange', 
-//     width: 25, 
-//     height: 25, 
-//     borderRadius: 25, 
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     marginRight: 15,
-//   },
-// });
-//--------------------------------------
-
-
-
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, Platform, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'; 
 import FirstScreen from './firstScreen'; 
 
-const { width, height } = Dimensions.get('window');
 const Stack = createStackNavigator();
 
 function ProgramDetailsScreen() {
@@ -136,13 +28,11 @@ function ProgramDetailsScreen() {
           Your Be future ready program has been successfully purchased
         </Text>
 
-        
         <View style={styles.featureContainer}>
           <Ionicons name="checkmark-circle" size={24} color="#191970" /> 
           <Text style={styles.featureText}>This program has 6 activities</Text>
         </View>
 
-        
         <View style={styles.featureContainer}>
           <Ionicons name="checkmark-circle" size={24} color="#191970" /> 
           <Text style={styles.featureText}>Full-time access to this program</Text>
@@ -187,7 +77,7 @@ function App() {
           component={FirstScreen} 
           options={{ 
             title: 'Be future ready',
-            headerRight: () => <PlayPauseButton />,  
+            headerRight: () => <PlayPauseButton />,
           }} 
         />
       </Stack.Navigator>
@@ -196,8 +86,8 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#f5f5f5', paddingTop: Platform.OS === 'ios' ? 15 : 0 },
-  programContainer: { alignItems: 'center', marginBottom: 30, height: height * 0.4, width: '100%', backgroundColor: '#f5f5dc', borderBottomRightRadius: 150, borderBottomLeftRadius: 150 },
+  container: { flex: 1, backgroundColor: '#f5f5f5', paddingTop: 0 },
+  programContainer: { alignItems: 'center', marginBottom: 30, height: 300, width: '100%', backgroundColor: '#f5f5dc', borderBottomRightRadius: 150, borderBottomLeftRadius: 150 },
   programImage: { width: 250, height: 250, borderRadius: 20 },
   programTitle: { fontSize: 18, color: 'black', marginTop: 10, fontWeight: 'bold' },
   detailsContainer: { marginTop: 20, marginBottom: 20, backgroundColor: 'transparent', borderRadius: 10, paddingHorizontal: 20 },
@@ -216,7 +106,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center',
     marginRight: 15,
-
   },
 });
 
--- firstScreen.js
+++ firstScreen.js
@@ -1,43 +1,56 @@
-
 import React, { useState } from 'react';
 import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
-import { BlurView } from 'expo-blur'; 
+import { BlurView } from 'expo-blur';
 
 const initialData = [
-  { id: '1', title: 'Day 1 - Deciding what you want', image: require('./assets/day1.webp'), bright: true, completed: false },
-  { id: '2', title: 'Day 2 - Transforming actions into habits', image: require('./assets/day2.jpg'), bright: false, completed: false },
-  { id: '3', title: 'Day 3 - Transforming actions into habits', image: require('./assets/day3.jpeg'), bright: false, completed: false },
-  { id: '4', title: 'Day 4 - Distraction: The actual reward', image: require('./assets/day4.jpg'), bright: false, completed: false },
-  { id: '5', title: 'Day 5 - Clear the chaos', image: require('./assets/day5.jpg'), bright: false, completed: false },
-  { id: '6', title: 'Day 6 - Bouncing back from setbacks', image: require('./assets/day6.jpeg'), bright: false, completed: false },
-  { id: '7', title: 'Day 7 - Finding your inner friend', image: require('./assets/day7.jpg'), bright: false, completed: false },
-  { id: '8', title: 'Day 8 - Reinforcing your habits', image: require('./assets/day8.jpg'), bright: false, completed: false }, 
+  { id: '1', title: 'Day 1 - Deciding what you want', image: require('./assets/day1.webp'), completed: false },
+  { id: '2', title: 'Day 2 - Transforming actions into habits', image: require('./assets/day2.jpg'), completed: false },
+  { id: '3', title: 'Day 3 - Transforming actions into habits', image: require('./assets/day3.jpeg'), completed: false },
+  { id: '4', title: 'Day 4 - Distraction: The actual reward', image: require('./assets/day4.jpg'), completed: false },
+  { id: '5', title: 'Day 5 - Clear the chaos', image: require('./assets/day5.jpg'), completed: false },
+  { id: '6', title: 'Day 6 - Bouncing back from setbacks', image: require('./assets/day6.jpeg'), completed: false },
+  { id: '7', title: 'Day 7 - Finding your inner friend', image: require('./assets/day7.jpg'), completed: false },
+  { id: '8', title: 'Day 8 - Reinforcing your habits', image: require('./assets/day8.jpg'), completed: false },
 ];
 
-const ListItem = ({ item, index, toggleCompletion, isDisabled }) => {
-  const fadeLevels = ['#fff', '#f0f0f0', '#e0e0e0', '#d0d0d0', '#c0c0c0', '#b0b0b0', '#a0a0a0'];
-  const backgroundColor = item.bright ? ['#fff', '#fff'] : (index === 7 ? ['#e6e6e6', '#e6e6e6'] : [fadeLevels[index], '#e6e6e6']);
+const getBackgroundColor = (index) => {
+  const colors = [ '#3271a6', '#3982b8', '#4194cb', '#46a2da', '#58afdd', '#6abce2', '#8dcfec', '#b8e2f4' ];
+  return colors[index] || '#b8e2f4'; 
+};
+
+const ListItem = ({ item, index, toggleCompletion, isDisabled, isSelected, onPress }) => {
+  const backgroundColor = getBackgroundColor(index);
 
   return (
     <TouchableOpacity
-      onPress={() => !isDisabled && toggleCompletion(item.id)}
+      onPress={onPress}
       style={[styles.itemContainer, isDisabled && styles.disabled]}
       disabled={isDisabled}
     >
       <View style={styles.itemContent}>
-        <LinearGradient colors={backgroundColor} style={styles.linearGradient}>
+        <View style={[styles.solidBackground, { backgroundColor }]}>
           <View style={styles.itemInnerContent}>
             <Image source={item.image} style={styles.image} />
-            <Text style={styles.title}>{item.title}</Text>
-            {item.completed ? (
-              <Ionicons name="checkmark-circle" size={24} color="orange" />
-            ) : (
-              <Ionicons name="lock-closed-outline" size={24} color="#ccc" />
-            )}
+            <View style={styles.textContainer}>
+              <Text style={styles.label}>Innergy</Text> 
+              <Text style={styles.title}>{item.title}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <View style={styles.iconBackground}>
+                {item.completed ? (
+                  <View style={styles.completedIcon}>
+                    <Ionicons name="checkmark-circle" size={24} color="#ff8c00" />
+                  </View>
+                ) : (
+                  <View style={styles.lockIcon}>
+                    <Ionicons name="lock-closed-outline" size={18} color="grey" />
+                  </View>
+                )}
+              </View>
+            </View>
           </View>
-        </LinearGradient>
+        </View>
         {isDisabled && (
           <BlurView style={styles.blurView} intensity={50} />
         )}
@@ -48,27 +61,30 @@ const ListItem = ({ item, index, toggleCompletion, isDisabled }) => {
 
 export default function SecondPage() {
   const [data, setData] = useState(initialData);
+  const [selectedId, setSelectedId] = useState(null);
 
-  const toggleCompletion = (id) => {
+  const toggleCompletion = (id, index) => {
     const updatedData = data.map((item) => {
       if (item.id === id) {
-    
         return { ...item, completed: !item.completed };
       }
       return item;
     });
 
     setData(updatedData);
+    setSelectedId(id);
   };
 
-  const getPreviousDayCompletionStatus = (index) => {
-    if (index === 0) return true;
-    return data[index - 1].completed; 
+  const onPressItem = (id, index) => {
+    if (!data[index].completed && (index === 0 || data[index - 1].completed)) {
+      toggleCompletion(id, index);
+    } else if (data[index].completed) {
+      toggleCompletion(id, index);
+    }
   };
 
   return (
     <View style={styles.container}>
-      
       <Text style={styles.subHeading}>
         These 8 activities will help you build a reading habit and improve your communication
       </Text>
@@ -80,9 +96,10 @@ export default function SecondPage() {
           <ListItem
             item={item}
             index={index}
-            toggleCompletion={toggleCompletion}
-            isPreviousCompleted={getPreviousDayCompletionStatus(index)}
-            isDisabled={index > 0 && !getPreviousDayCompletionStatus(index) && item.completed === false}
+            toggleCompletion={() => toggleCompletion(item.id, index)}
+            isSelected={selectedId === item.id}
+            onPress={() => onPressItem(item.id, index)}
+            isDisabled={index > 0 && !data[index - 1].completed && !item.completed}
           />
         )}
         contentContainerStyle={styles.list}
@@ -97,35 +114,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#f4f4f4',
     paddingTop: 50,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    marginBottom: 20,
-  },
-  mainHeading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 10,
-  },
   subHeading: {
     fontSize: 16,
     color: '#777',
     paddingHorizontal: 20,
     marginBottom: 40,
-    marginTop: -30
+    marginTop: -30,
   },
   list: {
     paddingHorizontal: 20,
   },
   itemContainer: {
-    marginBottom: 15,
+    marginBottom: 5,
   },
-  linearGradient: {
+  solidBackground: {
     borderRadius: 15,
     padding: 15,
+    overflow: 'hidden',
   },
   itemInnerContent: {
     flexDirection: 'row',
@@ -138,8 +143,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginRight: 15,
   },
-  title: {
+  textContainer: {
     flex: 1,
+    flexDirection: 'column',
+  },
+  label: {
+    fontSize: 14,
+    color: 'black', 
+  },
+  title: {
     fontSize: 16,
     color: '#333',
   },
@@ -153,5 +165,50 @@ const styles = StyleSheet.create({
     bottom: 0,
     height: 50,
     borderRadius: 15,
+  },
+  iconContainer: {
+    width: 50,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconBackground: {
+    backgroundColor: '#FFF',
+    height: 60,
+    width: 35,
+    borderRadius: 15,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  completedIcon: {
+    width: 27,
+    height: 27,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 27,
+    borderWidth: 0.7,
+    borderColor: 'transparent',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  lockIcon: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    borderWidth: 0.7,
+    borderColor: 'transparent',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
